5a10e1b856d25463540a6ef3600127f295b6d2bc
Abhinav Garg <abhinav.garg.iitk@gmail.com>
2016-12-23 12:38:28 +0530

    roughly tested version 1

--- main.cpp
+++ main.cpp
@@ -2,8 +2,168 @@
 #include "trie.h"
 
 
-int main() {
+bool isLegal(char c)
+{
+    char legal[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','I' /*..and so on*/};
+    int len = sizeof(legal)/sizeof(char);
 
+    for (int i = 0; i < len; i++)
+        if (c == legal[i])
+            return true;
+    return false;
+}
+
+bool compare(const pair<string,int> &a,const pair<string,int> &b)
+{
+    return a.second > b.second;
+}
+
+
+void split(const string &s, char delim, vector<string> &elems) {
+    stringstream ss;
+    ss.str(s);
+    string item;
+    while (getline(ss, item, delim)) {
+
+        elems.push_back(item);
+    }
+
+}
+
+
+void testDryRun(Trie t){
+
+    cout << endl << "*****************Dry-Run**************" << endl << endl;
+
+
+    t.printTree();
+
+    vector<string> user_input;
+    isDebug = false;
+
+    int count = 0;
+
+    while(1){
+        string curr_input = "";
+        cout << "user: ";
+        getline(cin, curr_input);
+
+        if(curr_input.length() == 0) {
+            //cout << "1" << endl;
+            if(count == 2)
+                t.printTree();
+            if (count >= 3)
+                break;
+            count++;
+            user_input.clear();
+        }
+        else{
+
+            count = 0;
+            //split(curr_input, ' ', user_input); //split by space
+            user_input.push_back(curr_input);
+
+            vector<pair<string, int>> out;
+            vector<string> predictions;
+
+            if(user_input.size() == 3){
+
+                cout << "user input order: " << user_input[0] << " " << user_input[1] << " " << user_input[2] << endl;
+
+                t.setTrigram(user_input[0], user_input[1], user_input[2]);
+            }
+
+            while(user_input.size() > 2)
+                user_input.erase(user_input.begin());
+
+            if(user_input.size() == 2){
+
+                cout << "user input order: " << user_input[0] << " " << user_input[1] << endl;
+
+                if(t.getTrigram(user_input[0], user_input[1], out)){
+                    cout << "Predictions for " << user_input[0] << " " << user_input[1] << endl;
+
+                    for(int i = 0; i < out.size(); i++){
+                        //cout << out[i].first << ", ";
+                        predictions.push_back(out[i].first);
+                        if(predictions.size()==3)
+                            break;
+                    }
+
+                    if(predictions.size() < 3){
+                        int n = (int)out.size();
+                        cout << "value of n: " << n << endl;
+                        out.clear();
+                        if(t.getBigram(user_input[1], out)){
+                            sort(out.begin(), out.end(), compare);
+                            for(int i = 0; i < out.size(); i++){
+                                //cout << out[i].first << ", ";
+                                if(find(predictions.begin(), predictions.end(), out[i].first)-predictions.begin() >= predictions.size())
+                                    predictions.push_back(out[i].first);
+
+                                if(predictions.size()==3)
+                                    break;
+                            }
+                        }
+                    }
+
+                    cout << endl;
+
+                }
+                else{
+                    if(t.getBigram(user_input[1], out)){
+                        sort(out.begin(), out.end(), compare);
+                        cout << "Predictions for " << user_input[1] << endl;
+                        for(int i = 0; i < out.size(); i++){
+                            //cout << out[i].first << ", ";
+                            predictions.push_back(out[i].first);
+                            if(predictions.size()==3)
+                                break;
+                        }
+                    }
+                }
+            }
+
+            if(user_input.size()==1){
+                if(t.getBigram(user_input[0], out)){
+                    sort(out.begin(), out.end(), compare);
+                    cout << "Predictions for " << user_input[0] << endl ;
+                    for(int i = 0; i < out.size(); i++){
+                        //cout << out[i].first << ", ";
+                        predictions.push_back(out[i].first);
+                        if(predictions.size()==3)
+                            break;
+                    }
+                }
+            }
+
+
+            if(predictions.empty())
+                continue;
+
+            //show predictions
+            for(int i = 0; i < predictions.size()-1; i++)
+                cout << predictions[i] << ", ";
+            cout << predictions[predictions.size()-1] << endl;
+        }
+
+    }
+}
+
+
+void makeTree(Trie &t){
+
+    string unigramFile = "/media/abu/DATA/fromUbuntu/Bobble/keyboard/next_word_prediction/mastodon/newdata/output/unigrams.txt";
+    string bigramFile = "/media/abu/DATA/fromUbuntu/Bobble/keyboard/next_word_prediction/mastodon/newdata/output/ngrams2.ll";
+    string trigramFile = "/media/abu/DATA/fromUbuntu/Bobble/keyboard/next_word_prediction/mastodon/newdata/output/ngrams3.ll";
+
+
+    //read from unigram file and add
+
+}
+
+
+int main() {
 
     //initialize tree
     Trie t;
@@ -18,17 +178,26 @@ int main() {
         parentNode->children.push_back(cnode);
         parentNode = cnode;
 
-        cout << "added: " << cnode->value << endl;
-        cout << "parent: " << cnode->parent->value << endl;
+        //cout << "added: " << cnode->value << endl;
+        //cout << "parent: " << cnode->parent->value << endl;
     }
-
-    cout << "checking for added word" << endl;
+    t.trieWords.push_back(parentNode);
+    //cout << "checking for added word" << endl;
 
     bool isWordExist = false;
     std::shared_ptr<cNode> wordNode = t.root;
     t.getSetWordNode("hello", isWordExist, wordNode);
 
-    cout << "leaf node char: " << wordNode->value << endl;
+    //cout << "leaf node char: " << wordNode->value << endl;
+
+    vector<pair<string, int>> results;
+    if(!t.getTrigram("hello", "world", results))
+        t.getBigram("hello", results);
+
+    for(int i = 0; i < results.size(); i++)
+        cout << "predictions for hello: " << results[i].first << endl;
+
+
 
    /* cout << "parent of leaf node char: " << (int)wordNode->parent->value << endl;
 
--- trie.h
+++ trie.h
@@ -20,7 +20,7 @@ vector<string> words;  //or could be vector of pointers to word in a dictionary
 
 int dummy = 0; //dummy weight
 
-bool isDebug = true;
+bool isDebug = false;
 
 template <typename T>
 string to_string(T value)
@@ -43,6 +43,7 @@ void LOG(string msg){
 class cNode{
 public:
     char value;  // character value of node
+    int unigramWeight;
     vector<std::shared_ptr<cNode>> children; //character nodes connected to this character node
     vector<std::shared_ptr<cNode>> bigrams;  //<word1, weight1>, <word2, weight2>, ..
     vector<int> bigramWeights;
@@ -53,6 +54,7 @@ public:
 
     cNode(){
         value = 0;
+        unigramWeight = 0;
         parent = nullptr;
     };
 };
@@ -62,6 +64,8 @@ class Trie {
 public:
 
     std::shared_ptr<cNode> root;
+
+    vector<shared_ptr<cNode>> trieWords;
 
     Trie(){
 
@@ -77,13 +81,52 @@ public:
     };
 
     string getWord(std::shared_ptr<cNode> cnode);
+
     void getSetWordNode(const string word, bool &isWordExist, std::shared_ptr<cNode> &wordnode);
 
     bool getBigram(const string head, vector<pair<string, int>> &results);
 
     bool getTrigram(const string head1, const string head2, vector<pair<string, int>> &results);
 
+    void setTrigram(const string head1, const string head2, const string word);
+
+    void printTree();
+
 };
+
+
+void Trie::printTree(){
+
+
+    cout << "*****************TRIE******************"<< endl << endl;
+    cout << "Words in Trie: " << endl << endl;
+
+    for(int i = 0; i < this->trieWords.size(); i++) {
+
+        shared_ptr<cNode> cnode = this->trieWords[i];
+
+        cout << getWord(cnode) << endl;
+
+        if(!cnode->bigrams.empty()){
+            cout << "Bigrams: " << endl;
+            for(int j = 0; j < cnode->bigrams.size(); j++)
+                cout << "      " << getWord(cnode->bigrams[j]) << ", " << cnode->bigramWeights[j] << endl;
+        }
+
+        if(!cnode->trigrams.empty()){
+            cout << "Trigrams: " << endl;
+            for(int j = 0; j < cnode->trigrams.size(); j++){
+                cout << getWord(cnode->trigrams[j][0]) << ": " << endl;
+                for(int k = 1; k < cnode->trigrams[j].size(); k++)
+                    cout << "       " << getWord(cnode->trigrams[j][k]) << "  " << cnode->trigramWeights[j][k-1] << endl;
+            }
+        }
+
+    }
+
+    cout << "**************************************"<< endl;
+
+}
 
 //reverse traverse tree to find word represented by a leaf node
 string Trie::getWord(std::shared_ptr<cNode> cnode){
@@ -94,7 +137,7 @@ string Trie::getWord(std::shared_ptr<cNode> cnode){
     while(cnode->parent!= nullptr){
         word = word+cnode->value;
 
-        cout << "word: " << word << endl;
+        //cout << "word: " << word << endl;
 
         cnode = cnode->parent;
 
@@ -108,9 +151,13 @@ string Trie::getWord(std::shared_ptr<cNode> cnode){
     if(word.length()==1)
         return word;
 
+    //cout << "word: " << word << endl;
+
     string reverseWord = "";
-    for(int i = word.length()-1; i == 0; i--)
+    for(int i = word.length()-1; i > -1; i--)
         reverseWord = reverseWord + word[i];
+
+    //cout << "reverse word: " << reverseWord << endl;
 
     return reverseWord;
 }
@@ -162,9 +209,9 @@ void Trie::getSetWordNode(const string word, bool &isWordExist, std::shared_ptr<
             LOG(childNode.value);
             if(currNode->children.size() > 0) {
                 for (int j = 0; j < currNode->children.size(); j++) {
-                    cout << "child char value: " << currNode->children[j]->value << endl;
+                    //cout << "child char value: " << currNode->children[j]->value << endl;
                     if (currNode->children[j]->value == c) {
-                        cout << "match found: " << endl;
+                        //cout << "match found: " << endl;
                         break;
                     }
                     count++;
@@ -205,10 +252,20 @@ void Trie::getSetWordNode(const string word, bool &isWordExist, std::shared_ptr<
         }
     }
 
-    LOG("word added");
+    if(!isWordExist) {
+        this->trieWords.push_back(currNode);
+        currNode->unigramWeight = 1;
+        cout << "word added to trie: " << word << endl;
+        LOG("word added");
+    }
+    else
+        currNode->unigramWeight = max(255, currNode->unigramWeight+1);
+
 
     LOG("parent of leaf node: ");
     LOG(currNode->parent->value);
+
+
 }
 
 bool Trie::getBigram(const string head, vector<pair<string, int>> &results) {
@@ -221,13 +278,12 @@ bool Trie::getBigram(const string head, vector<pair<string, int>> &results) {
         if(!headNode->bigrams.empty())
             for(int i = 0; i < headNode->bigrams.size(); i++)
                 results.push_back(make_pair(getWord(headNode->bigrams[i]), headNode->bigramWeights[i]));
-        else
-            cout << "no bigrams found for the head: " << head << endl;
+        /*else
+            cout << "no bigrams found for the head: " << head << endl;*/
     }
-    else{
-        cout << head << " does not exist in trie .. head is added to trie";
-    }
-
+    /*else{
+        //cout << head << " does not exist in trie .. head is added to trie";
+    }*/
 
     return !results.empty();
 
@@ -244,23 +300,104 @@ bool Trie::getTrigram(const string head1, const string head2, vector<pair<string
     getSetWordNode(head2, isHead2Exist, headNode2);
 
     if(!isHead1Exist || !isHead2Exist) {
-        cout << "head do not exist in trie ... head added to trie" << endl;
+        //cout << "head do not exist in trie ... head added to trie" << endl;
         //head 1 do not exist and is added --> add head2 as bigram for head1
         headNode1->bigrams.push_back(headNode2);
         headNode1->bigramWeights.push_back(dummy);
-        cout << "head2 is added as bigram for head1" << endl;
+        //cout << "head2 is added as bigram for head1" << endl;
         return false;
     }
 
     if(isHead1Exist && isHead2Exist){
+
+        if(headNode1->bigrams.empty()){
+            headNode1->bigrams.push_back(headNode2);
+            headNode1->bigramWeights.push_back(dummy);
+        }
+        else {
+            //update weight for bigram of headNode1
+            for (int i = 0; i < headNode1->bigrams.size(); i++) {
+
+                long pos = find(headNode1->bigrams.begin(), headNode1->bigrams.end(), headNode2) -
+                           headNode1->bigrams.begin();
+
+                if (pos >=
+                    headNode1->bigrams.size()) { //headNode2 does not exist in bigrams of headNode1 --> add to bigram
+                    headNode1->bigrams.push_back(headNode2);
+                    headNode1->bigramWeights.push_back(dummy);
+                } else
+                    headNode1->bigramWeights[pos] = max(255, headNode1->bigramWeights[pos]+1); //update weight
+            }
+        }
+
+        //get trigram result
         for(int i = 0; i < headNode1->trigrams.size(); i++){
             if(headNode2 == headNode1->trigrams[i][0]){
                 for(int j = 1; j < headNode1->trigrams[i].size(); j++){
-                    results.push_back(make_pair(getWord(headNode1->trigrams[i][j]), headNode1->trigramWeights[i][j]));
+                    results.push_back(make_pair(getWord(headNode1->trigrams[i][j]), headNode1->trigramWeights[i][j-1]));
                 }
             }
         }
     }
 
    return !results.empty();
+}
+
+
+void Trie::setTrigram(const string head1, const string head2, const string word){
+
+    bool isHead1Exist, isHead2Exist = true;
+
+    std::shared_ptr<cNode> headNode1 = this->root;
+    std::shared_ptr<cNode> headNode2 = this->root;
+
+    getSetWordNode(head1, isHead1Exist, headNode1);
+    getSetWordNode(head2, isHead2Exist, headNode2);
+
+    if(!isHead1Exist || !isHead2Exist) {
+        //cout << "head do not exist in trie ... head added to trie" << endl;
+        //head 1 do not exist and is added --> add head2 as bigram for head1
+        headNode1->bigrams.push_back(headNode2);
+        headNode1->bigramWeights.push_back(dummy);
+        //cout << "head2 is added as bigram for head1" << endl;
+    }
+
+    bool isWordExist = false;
+    shared_ptr<cNode> wordNode = this->root;
+    getSetWordNode(word, isWordExist, wordNode);
+
+    if(headNode1->trigrams.empty()) {
+        vector<shared_ptr<cNode>> trigramVec;
+        trigramVec.push_back(headNode2);
+        trigramVec.push_back(wordNode);
+        vector<int> trigramWeights;
+        trigramWeights.push_back(dummy);
+        headNode1->trigrams.push_back(trigramVec);
+        headNode1->trigramWeights.push_back(trigramWeights);
+    }
+    else{
+        bool addedToTrigram = false;
+        for(int i = 0; i < headNode1->trigrams.size(); i++){
+            if(headNode2 == headNode1->trigrams[i][0]) {
+                addedToTrigram = true;
+                long pos = find(headNode1->trigrams[i].begin(), headNode1->trigrams[i].end(), wordNode)-headNode1->trigrams[i].begin();
+                if( pos >= headNode1->trigrams[i].size()){
+                    headNode1->trigrams[i].push_back(wordNode);
+                    headNode1->trigramWeights[i].push_back(dummy);
+                }
+                else{
+                    headNode1->trigramWeights[i][pos] = max(255, headNode1->trigramWeights[i][pos]+1); //update weight
+                }
+            }
+        }
+
+        if(!addedToTrigram){
+            vector<shared_ptr<cNode>> newTrigram;
+            newTrigram.push_back(headNode2);
+            newTrigram.push_back(wordNode);
+            headNode1->trigrams.push_back(newTrigram);
+        }
+
+    }
+
 }
